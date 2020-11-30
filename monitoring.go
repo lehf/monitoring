9c4e53ef0f2f6f552cf7e53a30ea58e5abd752c1
lehf <1764129339@qq.com>
2020-11-30 08:49:20 +0800

    弹窗消息

--- monitoring.go
+++ monitoring.go
@@ -3,21 +3,36 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
 	"net/http"
 	"path/filepath"
-	"io"
-	"bytes"
+	"io/ioutil"
   	"github.com/PuerkitoBio/goquery"
 	"time"
-	// "github.com/lxn/walk"
-	// . "github.com/lxn/walk/declarative"
+	. "github.com/lxn/walk/declarative"
 )
+
+var time1 int
+
 
 
 func main() {
+
+	path, _ := os.Executable()
+	dir := filepath.Dir(path)
+
+	time_bytes, time_text_err := ioutil.ReadFile(dir+"\\time.txt")
+
+	if time_text_err == nil{
+	    time1, _ = strconv.Atoi(string(time_bytes))
+	}else{
+	 	time1 = 10
+	}
+
+
 	var ch chan int
 	//定时任务
-	ticker := time.NewTicker(time.Second * 5)
+	ticker := time.NewTicker(time.Second * time.Duration(time1))
 	go func() {
 			for range ticker.C {
 				opFile()
@@ -40,101 +55,93 @@ func opFile(){
 
 
 	_, older1_err := os.Stat(older1_path)
-	fmt.Println(older1_err)
+	if older1_err != nil {
+			fmt.Println(older1_err)
+	}
 
 	if older1_err == nil {
 		os.Remove(older1_path)
 	}
 
 	_, older2_err := os.Stat(older2_path)
-	fmt.Println(older2_err)
+	if older2_err != nil {
+		fmt.Println(older2_err)
+}
 
 	if older2_err == nil {
 		os.Rename(older2_path, older1_path)
 	}
 
 	_, err := os.Stat(new_path)
-	fmt.Println(err)
+	if err != nil {
+		fmt.Println(err)
+}
 	if err == nil{
 		re_err := os.Rename(new_path, older2_path)
-		fmt.Println(re_err)
+		if re_err !=nil{
+			fmt.Println(re_err)
+		}
+	}else{
+		fmt.Println(err)
 
-	}		
-	os.Create(dir+"\\new.txt")
+	}	
+	f, _ := os.Create(dir+"\\new.txt")
+	f.Close()
 	spider(new_path)
 
-	if compare(new_path, older2_path) != true {
+	if compare(new_path, older2_path) == true {
 		fmt.Println("数据有更新！请查看")
-		
+		message()
+	}else{
+		fmt.Println("数据暂无更新")
 	}
-}
-
-func compareLines(line1, line2 string) {
-	sign := "o"
-	if line1 != line2 {
-		sign = "x"
-	}
-	fmt.Printf("%s | %s | %s n", sign, line1, line2)
 }
 
 // 对比是否有更新
 func compare(spath, dpath string) bool {
-	sinfo, err := os.Lstat(spath)
-	if err != nil {
-			return false
-	}
-	dinfo, err := os.Lstat(dpath)
-	if err != nil {
-			return false
-	}
-	if sinfo.Size() != dinfo.Size() || !sinfo.ModTime().Equal(dinfo.ModTime()) {
-			return false
-	}
-	return comparefile(spath, dpath)
-}
 
-func comparefile(spath, dpath string) bool {
-	sFile, err := os.Open(spath)
-	if err != nil {
-			return false
+	text1, err1 := ioutil.ReadFile(spath)
+	text2, err2 := ioutil.ReadFile(dpath)
+
+    if err1 != nil {
+        fmt.Println("error : %s", err1)
 	}
-	dFile, err := os.Open(dpath)
-	if err != nil {
-			return false
+	if err2 != nil {
+        fmt.Println("error : %s", err2)
+    }
+	
+	
+	if string(text1) == string(text2) {
+		return false
+	}else{
+		return true
 	}
-	b := comparebyte(sFile, dFile)
-	sFile.Close()
-	dFile.Close()
-	return b
-}
-//下面可以代替md5比较.
-func comparebyte(sfile *os.File, dfile *os.File) bool {
-	var sbyte []byte = make([]byte, 512)
-	var dbyte []byte = make([]byte, 512)
-	var serr, derr error
-	for {
-			_, serr = sfile.Read(sbyte)
-			_, derr = dfile.Read(dbyte)
-			if serr != nil || derr != nil {
-					if serr != derr {
-							return false
-					}
-					if serr == io.EOF {
-							break
-					}
-			}
-			if bytes.Equal(sbyte, dbyte) {
-					continue
-			}
-			return false
-	}
-	return true
+
 }
 
 // 爬取
 func spider(path string) {
-	url := "https://tieba.baidu.com/f?kw=%E6%AD%A6%E6%B1%89"
-	result, _ := HttpGet(url)
+	var url string
+	current_path, _ := os.Executable()
+	dir := filepath.Dir(current_path)
+	url_bytes, url_err := ioutil.ReadFile(dir+"\\url.txt")
+
+	if url_err == nil{
+	    url = string(url_bytes)
+	}else{
+		url = ""
+	}
+	if url== ""{
+		fmt.Println("请输入要监测的网址！！！！")
+	}
+
+	result, get_err := HttpGet(url)
+	if get_err != nil {
+		fmt.Printf("抓取错误：%s\n", get_err )
+	}else{
+		fmt.Printf("%s抓取\n",time.Now()   )
+
+	}
 	f, _ := os.OpenFile(path, os.O_RDWR, 6)
 	f.WriteString(result)
 	f.Close()
@@ -148,10 +155,22 @@ func HttpGet(url string) (result string, err error) {
 			return
 	}
 	defer resp.Body.Close()
-
 	doc, err := goquery.NewDocumentFromReader(resp.Body)
-	
-	doc.Find(".threadlist_title.pull_left.j_th_tit").Each(func(i int, s *goquery.Selection) {
+
+	var rule string
+	path, _ := os.Executable()
+	dir := filepath.Dir(path)
+	rule_bytes, rule_text_err := ioutil.ReadFile(dir+"\\rule.txt")
+
+	if rule_text_err == nil{
+	    rule = string(rule_bytes)
+	}else{
+	 	rule = ""
+	}
+	if rule == ""{
+		fmt.Println("请输入要监测的规则！！！！")
+	}
+	doc.Find(rule).Each(func(i int, s *goquery.Selection) {
     // For each item found, get the band and title
     band := s.Find("a").Text()
 		result += string(band)
@@ -162,3 +181,18 @@ func HttpGet(url string) (result string, err error) {
 
 
 // 弹窗提醒
+func message() {
+	var LableHello=Label{
+		Text: "网页有更新，请及时查看!", 
+	  }
+	  var widget=[]Widget{
+		 LableHello,
+	  }
+	  var mainWindow=MainWindow{
+		Title:"网页检测",
+		Size:Size{Width: 300, Height: 100},
+		Layout:VBox{}, 
+		Children:widget,
+	  }
+	  mainWindow.Run()
+}
